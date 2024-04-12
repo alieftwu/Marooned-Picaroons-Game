cd28c94ef826af9528d23ffdefdf72e68ad92e54
TheDuckLord2 <88846468+TheDuckLord2@users.noreply.github.com>
2024-04-12 15:34:58 -0500

    Work on NPCs and dialogue
    Added an NPC to the tutorial Town, and when "space" is pressed, a box will open with text

---
+++ Scripts/base_script.gd
@@ -0,0 +1,39 @@
+class_name BaseScene extends Node
+
+@onready var player : Player = $Player
+@onready var entrance_markers: Node2D = $EntranceMarkers
+
+@onready var boxText = get_node("Player/Camera2D/TextureRect")
+@onready var NPC = get_node("NPC")
+
+func _ready():
+	boxText.hide()
+	if scene_manager.player:
+		if player:
+			player.queue_free()
+			
+		player = scene_manager.player
+		add_child(player)
+	position_player()
+	
+func position_player() -> void:
+	var last_scene = scene_manager.last_scene_name
+	if last_scene.is_empty():
+		last_scene = "any"
+	for entrance in entrance_markers.get_children():
+		if entrance is Marker2D and entrance.name == "any" or entrance.name == last_scene:
+			player.global_position = entrance.global_position
+
+func _on_follow_node_set(node):
+	$Camera2D.follow_node = node
+
+
+func _on_npc_show_text_box():
+	boxText.show()
+	boxText.get_node("Label").text = NPC.npcText
+
+
+
+
+func _on_npc_hide_text_box():
+	boxText.hide()
