7da7ef0c1612edeadf28c344957da1314ead2b3d
TheDuckLord2 <88846468+TheDuckLord2@users.noreply.github.com>
2024-04-16 12:52:01 -0500

    changed not much but now it works

--- World/Label.gd
+++ World/Label.gd
@@ -18,15 +18,16 @@ func _process(delta):
 	if textBox.visible == true:
 		tween.tween_property(self, "visible_ratio", 2, 5)
 	else:
-		tween.tween_property(self, "visible_ratio", -1, 0.1)
+		tween.tween_property(self, "visible_ratio", -2, 0.1)
 		visible_ratio = 0
 		iterator = 0
 		text = NPC.npcText[0]
 	if textBox.visible == true and Input.is_action_just_pressed("ui_accept") and iterator < NPC.npcText.size()-1:
-		tween.tween_property(self, "visible_ratio", -1, 0.1)
+		tween.tween_property(self, "visible_ratio", -2, 0.1)
 		visible_ratio = 0
 		iterator += 1
 		text = NPC.npcText[iterator]
 		tween.tween_property(self, "visible_ratio", 2, 5)
-	elif textBox.visible == true and Input.is_action_just_pressed("ui_accept") and iterator == NPC.npcText.size():
+	elif textBox.visible == true and Input.is_action_just_pressed("ui_accept") and iterator == NPC.npcText.size()-1:
 		emit_signal("hideBoxLabel")
+		print("swap to scene for fighting")
