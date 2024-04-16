a141893b23a28598c0883dc52c93261742c05141
TheDuckLord2 <88846468+TheDuckLord2@users.noreply.github.com>
2024-04-15 21:18:53 -0500

    Added more NPC Logic

--- Scripts/base_script.gd
+++ Scripts/base_script.gd
@@ -7,7 +7,6 @@ class_name BaseScene extends Node
 @onready var NPC = get_node("NPC")
 
 func _ready():
-	boxText.hide()
 	if scene_manager.player:
 		if player:
 			player.queue_free()
@@ -31,9 +30,6 @@ func _on_follow_node_set(node):
 func _on_npc_show_text_box():
 	boxText.show()
 	boxText.get_node("Label").text = NPC.npcText[0]
-
-
-
 
 func _on_npc_hide_text_box():
 	boxText.hide()
