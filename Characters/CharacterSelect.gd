a35acfd6000f19fa5b7f7ca02063cc7528713e96
alieftwu <alieftwu@gmail.com>
2024-04-30 13:04:42 -0500

    Made character selection work w/ game

--- Characters/CharacterSelect.gd
+++ Characters/CharacterSelect.gd
@@ -2,18 +2,33 @@ extends Node2D
 
 
 func _process(delta):
-	match Game.PlayerSelect:
+	match Global.PlayerSelect:
 		0:
 			get_node("PlayerSelect").play("Player0")
 			get_node("Desc").text = "Special Abilities:\n-Can throw bomb and deal high damage of the enemy and any target near the enemy\n-Deals high damage when using ranged attacks  "
 		1:
 			get_node("PlayerSelect").play("Player1")
 			get_node("Desc").text = "Special Abilities:\n-Can resist ranged attacks\n-Deals extra damage when using melee weapon"
+		2:
+			get_node("PlayerSelect").play("Player2")
+			get_node("Desc").text = "Special Abilities:\n-Gets an extra turn when using melee\n-Deals extra damage when doing ranged attacks"
+		3:
+			get_node("PlayerSelect").play("Player3")
+			get_node("Desc").text = "Special Abilities:\n-Place Special Ability Here \n-Place Special Ability Here"
+		4:
+			get_node("PlayerSelect").play("Player4")
+			get_node("Desc").text = "Special Abilities:\n-Place Different Special Ability Here \n-Place Different Special Ability Here"
+		5:
+			get_node("PlayerSelect").play("Player5")
+			get_node("Desc").text = "Special Abilities:\n-Place Another Special Ability Here \n-Place Another Special Ability Here"
 func _on_left_pressed():
-	if Game.PlayerSelect >= 0:
-		Game.PlayerSelect -= 1
+	if Global.PlayerSelect >= 0:
+		Global.PlayerSelect -= 1
 
 
 func _on_right_pressed():
-	if Game.PlayerSelect < 2:
-		Game.PlayerSelect += 1
+	if Global.PlayerSelect < 6:
+		Global.PlayerSelect += 1
+
+func _on_select_pressed():
+	get_tree().change_scene_to_file("res://World/world.tscn")
