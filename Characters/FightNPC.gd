2b65412436f457a22d6f0b7ab6cb26561d6a3e52
alieftwu <alieftwu@gmail.com>
2024-05-02 19:46:02 -0500

    manual merge sam

---
+++ Characters/FightNPC.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+@onready var around = get_node("Area2D")
+@onready var bubble = get_node("TextureRect")
+@onready var textBox = get_node("HUD/TextureRect")
+@onready var textBoxText = get_node("HUD/TextureRect/Label")
+
+@export var fightScene = NodePath()
+@export var npcText = ["I say words! and when you press space, I say more!", "like this!", "Very cool I know.", "Now lets fight!"]
+
+signal showTextBox
+signal hideTextBox
+signal haltMove
+signal newFText(title, scene)
+
+var closeEnough = false
+var textDisplayed = false
+
+func _ready():
+	bubble.hide()
+	pass
+
+func _process(delta):
+	pass
+
+func _on_area_2d_body_entered(body):
+	if body != self and body != get_tree().current_scene.get_node("TileMap"):
+		closeEnough = true
+		haltMove.emit()
+		newFText.emit(self.name, fightScene)
+		showTextBox.emit()
+
+
+func _on_area_2d_body_exited(body):
+	emit_signal("hideTextBox")
+	closeEnough = false
+
