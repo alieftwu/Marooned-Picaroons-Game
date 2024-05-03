433634740d8434b55af7678daa47cdd1404a5e44
alieftwu <alieftwu@gmail.com>
2024-05-02 19:20:58 -0500

    Revert "merge sam branch into mine"
    This reverts commit ea9b2cafbca8c209da0af43b5ed58647afa02a4b, reversing
    changes made to 6229ba9b25309135343b0e42f5811fe0d91283ee.

--- World/Label.gd
+++ World/Label.gd
@@ -4,7 +4,6 @@ extends Label
 @onready var NPC = get_parent().get_parent().get_parent() 
 @onready var iterator = -1
 @onready var textList = [""]
-@onready var doFight = false
 
 # Called when the node enters the scene tree for the first time.
 signal hideBoxLabel
@@ -15,15 +14,14 @@ func _ready():
 	pass
 
 func _process(delta):
-	if textBox.visible and Input.is_action_just_pressed("ui_accept") and iterator < textList.size()-1:
+	if textBox.visible == true and Input.is_action_just_pressed("ui_accept") and iterator < textList.size()-1:
 		iterator += 1
 		text = textList[iterator]
-	elif textBox.visible and Input.is_action_just_pressed("ui_accept") and iterator == textList.size()-1:
+	elif textBox.visible == true and Input.is_action_just_pressed("ui_accept") and iterator == textList.size()-1:
 		emit_signal("hideBoxLabel")
 		print("swap to scene for fighting")
 		iterator = -1
-		if doFight:
-			scene_manager.change_scene(get_owner(), connected_scene)
+		# scene_manager.change_scene(get_owner(), connected_scene)
 
 
 func _on_new_text(title):
@@ -46,16 +44,3 @@ func _on_new_q_text(title, QS, QF):
 
 func _on_scene_transition():
 	print("here I am!")
-
-
-func _on_npc_halt_move():
-	iterator = 0
-
-
-func _on_new_f_text(title, scene):
-	NPC = NPC.get_parent().get_node(NodePath(title))
-	# connected_scene = scene
-	textList = NPC.npcText
-	text = textList[0]
-	doFight = true
-
