d8d272221de7e58ee2cbb16edec8a1b30a835480
alieftwu <alieftwu@gmail.com>
2024-05-04 18:23:56 -0500

    added  fight npc files

--- Characters/FightNPC.gd
+++ Characters/FightNPC.gd
@@ -6,17 +6,32 @@ extends CharacterBody2D
 @onready var textBoxText = get_node("HUD/TextureRect/Label")
 @onready var animation_tree : AnimationTree = $AnimationTree
 
-@export var npcText = ["I say words! and when you press space, I say more!", "like this!", "Very cool I know.", "Now lets fight!"]
-@export var fightScene = NodePath()
+@export var direction = "down"
+@export var npcText = ["Now lets fight!"]
+
+
 signal showTextBox
 signal hideTextBox
 signal haltMove
-signal newFText(title, scene)
+signal newFText(title)
 
 var closeEnough = false
 var textDisplayed = false
 
 func _ready():
+	match direction:
+		"down":
+			animation_tree["parameters/Idle/blend_position"] = Vector2(0,1)
+			around.rotation = Vector2(1,0).angle()
+		"up":
+			animation_tree["parameters/Idle/blend_position"] = Vector2(0,-1)
+			around.rotation = Vector2(-1,0).angle()
+		"left":
+			animation_tree["parameters/Idle/blend_position"] = Vector2(-1,0)
+			around.rotation = Vector2(0,1).angle()
+		"right":
+			animation_tree["parameters/Idle/blend_position"] = Vector2(1,0)
+			around.rotation = Vector2(0,-1).angle()
 	bubble.hide()
 	pass
 
@@ -24,12 +39,12 @@ func _process(delta):
 	pass
 
 func _on_area_2d_body_entered(body):
-	if body != self and body != get_tree().current_scene.get_node("TileMap"):
+	print(body, " Entered")
+	if body != get_tree().current_scene.get_node("TileMap") and body != self:
 		closeEnough = true
 		haltMove.emit()
-		newFText.emit(self.name, fightScene)
+		newFText.emit(self.name)
 		showTextBox.emit()
-		
 
 func _on_area_2d_body_exited(body):
 	emit_signal("hideTextBox")
