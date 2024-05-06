eaedb285b90de82438f9c641456f7d5c47ebdfa5
Vishal Murugavel <103973106+vishalmuru21@users.noreply.github.com>
2024-05-05 21:30:03 -0500

    Lose Screen Items

---
+++ Lose Screen/Lose Screen.gd
@@ -0,0 +1,53 @@
+class_name LoseScreen
+extends Control
+
+
+@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+@onready var start_level = preload("res://World/world.tscn") as PackedScene
+
+
+func _ready():
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+	
+	
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+
+
+func on_exit_pressed() -> void:
+	get_tree().quit()
+	
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	
+func testEsc():
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
+		resume()
+		
+
+
+func _on_resume_pressed():
+	resume()
+
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+	
+func _process(delta):
+	testEsc()
+		
+		 
+		
+		
+
+
+	
